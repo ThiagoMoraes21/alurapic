efb49af372a4d248d8d96e0866a5c837e76dc791
thiagoMoraes21 <thiago4x70@gmail.com>
2020-09-27 02:27:59 -0300

    add a debounce strategy to the images search

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+
 import { Photo } from '../photo/photo';
 
 @Component({
@@ -7,10 +10,11 @@ import { Photo } from '../photo/photo';
 	templateUrl: './photo-list.component.html',
 	styleUrls: ['./photo-list.component.scss']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
 
 	public photos: Photo[] = [];
 	public filter: string = '';
+	public debounce: Subject<string> = new Subject<string>();
 
 	constructor(
 		private activatedRoute: ActivatedRoute
@@ -18,5 +22,13 @@ export class PhotoListComponent implements OnInit {
 
 	ngOnInit(): void {
 		this.photos = this.activatedRoute.snapshot.data.photos;
+
+		this.debounce
+			.pipe(debounceTime(300))
+			.subscribe(filter => this.filter = filter);
+	}
+
+	ngOnDestroy(): void {
+		this.debounce.unsubscribe();
 	}
 }
