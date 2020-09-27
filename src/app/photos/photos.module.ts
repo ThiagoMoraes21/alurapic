add58f7baa1dd7e7f98c8d6bcf85b10b6aae17f8
thiagoMoraes21 <thiago4x70@gmail.com>
2020-09-27 17:37:22 -0300

    add a module for each component

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -2,25 +2,15 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule, NO_ERRORS_SCHEMA } from "@angular/cor
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 
-import { PhotoListComponent } from './photo-list/photo-list.component';
-import { PhotoComponent } from './photo/photo.component';
-import { PhotoFormComponent } from './photo-form/photo-form.component';
-import { PhotosComponent } from './photo-list/photos/photos.component';
-import { FilterByDescription } from './photo-list/filter-by-description.pipe';
-import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
+import { PhotoModule } from './photo/photo.module';
+import { PhotoFormModule } from './photo-form/photo-form.module';
+import { PhotoListModule } from './photo-list/photo-list.module';
 
 @NgModule({
-    declarations: [ 
-        PhotoComponent,
-        PhotoListComponent,
-        PhotoFormComponent,
-        PhotosComponent,
-        FilterByDescription,
-        LoadButtonComponent
-    ],
     imports: [ 
-        HttpClientModule,
-        CommonModule
+        PhotoModule,
+        PhotoFormModule,
+        PhotoListModule
     ],
     schemas: [ 
         CUSTOM_ELEMENTS_SCHEMA,
