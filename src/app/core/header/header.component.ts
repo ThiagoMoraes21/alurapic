b619e786422e2a9a84047f2fd977b4b321208e71
thiagoMoraes21 <thiago4x70@gmail.com>
2020-10-01 02:15:20 -0300

    use async pipe to retrive users name on header

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../user/user';
 import { UserService } from '../user/user.service';
@@ -8,17 +8,11 @@ import { UserService } from '../user/user.service';
 	templateUrl: './header.component.html',
 	styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
 	public user$: Observable<User>;
-	public user: User;
 
 	constructor(userService: UserService) {
 		this.user$ = userService.getUser();
-		this.user$.subscribe(user => this.user = user);
 	}
-
-	ngOnInit(): void {
-	}
-
 }
