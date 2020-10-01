38f2b7397e632a5c846692a480036b34b32b24ab
thiagoMoraes21 <thiago4x70@gmail.com>
2020-10-01 01:15:58 -0300

    add token service

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { TokenService } from '../token/token.service';
 
 const API_URL = 'http://localhost:3000';
 
@@ -10,10 +12,21 @@ const API_URL = 'http://localhost:3000';
 export class AuthService {
 
 	constructor(
-		private http: HttpClient
+		private http: HttpClient,
+		private tokenService: TokenService
 	) { }
 
 	authenticate(userName: string, password: string): Observable<any> {
-		return this.http.post(API_URL + '/user/login', { userName, password });
+		return this.http
+			.post(
+				API_URL + '/user/login',
+				{ userName, password },
+				{ observe: 'response' }
+			)
+			.pipe(tap(res => {
+				const authToken = res.headers.get('x-access-token');
+				this.tokenService.setToken(authToken);
+			}));
+		
 	}
 }
