6cfdb4ce208fda84874a7ed12b3bbdcf3bfb127d
thiagoMoraes21 <thiago4x70@gmail.com>
2020-10-02 01:49:05 -0300

    add auth guard to check if user is logged

--- src/app/core/user/user.service.ts
+++ src/app/core/user/user.service.ts
@@ -10,6 +10,7 @@ import jwt_decode from "jwt-decode";
 export class UserService {
 
 	private userSubject = new BehaviorSubject<User>(null);
+	private userName: string;
 
 	constructor( private tokenService: TokenService) {
 		this.tokenService.hasToken() && this.decodeAndNotify();
@@ -27,11 +28,20 @@ export class UserService {
 	private decodeAndNotify(): void {
 		const token = this.tokenService.getToken();
 		const user = jwt_decode(token) as User;
+		this.userName = user.name;
 		this.userSubject.next(user);
 	}
 
 	logout(): void {
 		this.tokenService.removeToken();
 		this.userSubject.next(null);
+	}
+
+	isLogged(): boolean {
+		return this.tokenService.hasToken();
+	}
+
+	getUserName(): string {
+		return this.userName;
 	}
 }
